77c95749c5d2d0b0909ed25e0eb94893974d681b
Chad Kluck <clkluck@stthomas.edu>
2025-10-30 22:18:53 -0500

    fix: must use /home/fpp/ instead of ~ for mplayer script

--- AfterShowMusic-start.sh
+++ AfterShowMusic-start.sh
@@ -8,11 +8,8 @@
 # ./AfterShowMusic-start.sh christmas
 # ./AfterShowMusic-start.sh halloween
 
-# Ensure that FPP is not running
-sudo killall fpp
-
 # Ensure that MPlayer is not running
 sudo killall mplayer
 
 # Start Playback (shuffle, infinate loop, christmas is default if no arg passed to script)
-sudo mplayer -shuffle -loop 0 -playlist ~/media/aftershow/${1:-christmas}/playlist.txt
+sudo mplayer -shuffle -loop 0 -playlist /home/fpp/media/aftershow/${1:-christmas}/playlist.txt
